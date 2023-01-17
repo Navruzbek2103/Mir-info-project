6706e58e913990f6e858919b2f3b05782405f01f
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-01-17 17:41:29 +0300

    Comment icon add

--- src/pages/Today/index.jsx
+++ src/pages/Today/index.jsx
@@ -1,9 +1,9 @@
-
 import React from "react";
 import "./style.scss";
 import Img from "./../../assets/images/thumb-1.jpg";
 import { useState } from "react";
 import { FaFire } from "react-icons/fa";
+import { FaComment } from "react-icons/fa";
 
 const index = () => {
   return (
@@ -25,19 +25,25 @@ const index = () => {
                 className="img-today w-100 rounded-4"
                 alt="images"
               />
-              <div className="card-body position-absolute bottom-0 px-3 py-4">
+              <div className="card-body position-absolute bottom-0 px-3 py-4 w-100">
                 <h2 className="card__body--title text-white my-2 ">
                   Soprano Announces His New Album
                 </h2>
-                <div className="d-flex align-items-center gap-2">
-                  <img
-                    src={Img}
-                    width={50}
-                    height={50}
-                    className="rounded-5"
-                    alt="avatar"
-                  />
-                  <p className="text-white "> Elena Anka</p>
+                <div className="d-flex justify-content-between w-100">
+                  <div className="d-flex align-items-center gap-2">
+                    <img
+                      src={Img}
+                      width={50}
+                      height={50}
+                      className="rounded-5"
+                      alt="avatar"
+                    />
+                    <p className="text-white "> Elena Anka</p>
+                  </div>
+                  <div>
+                    <FaComment className="comment" />
+                    <span className="text-white mx-3">22</span>
+                  </div>
                 </div>
               </div>
             </div>
