e3b159feb0dd0f3fb41aceb33432119d5e5c0a31
Navruzbek2103 <navrozbek2306@gmail.com>
2023-01-20 01:51:55 +0500

    Added lifestyle section

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -6,6 +6,8 @@ import { NavLink, Link } from "react-router-dom";
 import PicsumImage2 from "./../../assets/images/thumb-15.jpg";
 import { AiFillStar } from "react-icons/ai";
 import PicsumImage3 from "./../../assets/images/future.jpg";
+import {CiClock2} from "react-icons/ci"
+
 
 const index = () => {
   return (
@@ -94,9 +96,12 @@ const index = () => {
             <h2 className="discover-authors__inner-title fw-semibold fs-4 text-capitalize">
               Popular authors
             </h2>
-            <button className="discover-authors__inner-btn text-uppercase text-white border-0 py-1 px-3 rounded-5">
+            <Link
+              to=""
+              className="discover-authors__inner-btn d-flex align-items-center  text-uppercase text-white border-0 py-1 px-3 rounded-5 fw-bold"
+            >
               See all
-            </button>
+            </Link>
           </div>
           <ul className="discover-authors__list d-flex flex-column gap-4">
             <li className="discover-authors__item">
@@ -234,6 +239,100 @@ const index = () => {
                 <h3 className="discover-fashion__item-link-title text-dark fs-5">
                   Best Clothes showed on fashion week
                 </h3>
+              </Link>
+            </li>
+          </ul>
+        </section>
+
+        <section className="discover-lifestyle border-top py-3">
+          <div className="discover-lifestyle__wrapper d-flex justify-content-between mb-3">
+            <h2 className="discover-lifestyle__wrapper-title text-capitalize fw-semibold">
+              In lifestyle
+            </h2>
+            <Link
+              to=""
+              className="discover-lifestyle__wrapper-btn text-uppercase text-white border-0 py-1 px-3 rounded-5 fw-bold d-flex align-items-center"
+            >
+              See all
+            </Link>
+          </div>
+          <ul className="discover-lifestyle__list d-flex flex-column gap-3">
+            <li className="discover-lifestyle__item">
+              <Link
+                to=""
+                className="discover-lifestyle__item-link  d-flex gap-3 pb-2"
+              >
+                <img
+                  src={PicsumImage3}
+                  alt="card img"
+                  className="discover-lifestyle__item-img rounded-2"
+                />
+                <div className="discover-lifestyle__item-inner border-bottom border-3 w-100">
+                  <h3 className="discover-lifestyle__item-inner-subtitle fw-bold fs-5 mb-2">
+                    Fashion
+                  </h3>
+                  <h4 className="discover-lifestyle__item-inner-title fw-bold text-dark mb-2">
+                    The Best Diet for a Flatter Belly
+                  </h4>
+                  <div className="discover-lifestyle__item-inner-box d-flex align-items-center gap-2">
+                    <CiClock2 className="d-flex flex-row-reverse" />
+                    <p className="discover-lifestyle__item-inner-box-text">
+                      2 hour ago
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+            <li className="discover-lifestyle__item">
+              <Link
+                to=""
+                className="discover-lifestyle__item-link  d-flex gap-3 pb-2"
+              >
+                <img
+                  src={PicsumImage3}
+                  alt="card img"
+                  className="discover-lifestyle__item-img rounded-2"
+                />
+                <div className="discover-lifestyle__item-inner border-bottom border-3 w-100">
+                  <h3 className="discover-lifestyle__item-inner-subtitle fw-bold fs-5 mb-2">
+                    Fashion
+                  </h3>
+                  <h4 className="discover-lifestyle__item-inner-title fw-bold text-dark mb-2">
+                    The Best Diet for a Flatter Belly
+                  </h4>
+                  <div className="discover-lifestyle__item-inner-box d-flex align-items-center gap-2">
+                    <CiClock2 className="d-flex flex-row-reverse" />
+                    <p className="discover-lifestyle__item-inner-box-text">
+                      2 hour ago
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+            <li className="discover-lifestyle__item">
+              <Link
+                to=""
+                className="discover-lifestyle__item-link  d-flex gap-3 pb-2"
+              >
+                <img
+                  src={PicsumImage3}
+                  alt="card img"
+                  className="discover-lifestyle__item-img rounded-2"
+                />
+                <div className="discover-lifestyle__item-inner border-bottom border-3 w-100">
+                  <h3 className="discover-lifestyle__item-inner-subtitle fw-bold fs-5 mb-2">
+                    Fashion
+                  </h3>
+                  <h4 className="discover-lifestyle__item-inner-title fw-bold text-dark mb-2">
+                    The Best Diet for a Flatter Belly
+                  </h4>
+                  <div className="discover-lifestyle__item-inner-box d-flex align-items-center gap-2">
+                    <CiClock2 className="d-flex flex-row-reverse"/>
+                    <p className="discover-lifestyle__item-inner-box-text">
+                      2 hour ago
+                    </p>
+                  </div>
+                </div>
               </Link>
             </li>
           </ul>
